e1ab39e333357881afd6c0fa6fc3a8dcfd062542
Cooper <104042225+thanksfortheride136@users.noreply.github.com>
2026-03-19 02:24:12 +0000

    init commmit

--- card_game.py
+++ card_game.py
@@ -1,1 +1,11 @@
+#Card Collector Game
 
+def greeting():
+    print("##################--Welcome to Battle 1.0--##################")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("Press C: Check Cards")
+    print("Press B: Buy Cards")
+    print("Press S: Sell Cards")
+    print("Press B: Battle Cards")
+    
+greeting()
